a17203e4f9a47e7bc8fabe77c4475027ee5a414a
NodCaleb <nod.caleb@gmail.com>
2023-05-24 10:16:15 +0300

    Initial

---
+++ MongoConsole/Animal.cs
@@ -0,0 +1,79 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoConsole
+{
+    public class Animal
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid Id { get; set; }
+        public string Species { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string Description => $"{Name} the {Species}, {Gender}, born {DateOfBirth.ToString("dd.MM.yyyy")}";
+
+        public static List<Animal> GenerateRandomAnimals(int count)
+        {
+            List<Animal> animals = new List<Animal>();
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                Animal animal = new Animal
+                {
+                    Id = Guid.NewGuid(),
+                    Species = GenerateRandomSpecies(),
+                    Name = GenerateRandomName(),
+                    Gender = GenerateRandomGender(),
+                    DateOfBirth = GenerateRandomDateOfBirth()
+                };
+
+                animals.Add(animal);
+            }
+
+            return animals;
+        }
+
+        public static string GenerateRandomSpecies()
+        {
+            string[] species = { "Dog", "Cat", "Bird", "Rabbit", "Fish" };
+            Random random = new Random();
+            int index = random.Next(species.Length);
+            return species[index];
+        }
+
+        public static string GenerateRandomName()
+        {
+            string[] names = { "Bella", "Max", "Charlie", "Luna", "Lucy", "Cooper", "Leo", "Oliver" };
+            Random random = new Random();
+            int index = random.Next(names.Length);
+            return names[index];
+        }
+
+        public static string GenerateRandomGender()
+        {
+            string[] genders = { "Male", "Female" };
+            Random random = new Random();
+            int index = random.Next(genders.Length);
+            return genders[index];
+        }
+
+        public static DateTime GenerateRandomDateOfBirth()
+        {
+            Random random = new Random();
+            int year = random.Next(2000, 2020);
+            int month = random.Next(1, 13);
+            int day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+            return new DateTime(year, month, day);
+        }
+
+    }
+}
---
+++ MongoConsole/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Driver.Core.Configuration;
+using MongoDB.Driver;
+using MongoConsole;
+using System.Xml.Linq;
+
+const string _databaseName = "Zoo";
+const string _collectionName = "Animals";
+
+Console.WriteLine("Trying Mongo!");
+
+var connectionString = "mongodb://user:password123@localhost:32768";
+
+var client = new MongoClient(connectionString);
+
+var databaseNames = client.ListDatabaseNames().ToList();
+
+foreach (var name in databaseNames)
+{
+    Console.WriteLine(name);
+}
+
+var database = client.GetDatabase(_databaseName);
+
+if (databaseNames.All(n => n != _databaseName))
+{
+    SeedDatabase(database);
+}
+
+ListAnimals(database);
+
+void SeedDatabase(IMongoDatabase database)
+{
+    var animals = Animal.GenerateRandomAnimals(100);
+
+    var animalsCollection = database.GetCollection<Animal>(_collectionName);
+
+    animalsCollection.InsertMany(animals);
+}
+
+void ListAnimals(IMongoDatabase database)
+{
+    var animalsCollection = database.GetCollection<Animal>(_collectionName);
+
+    Console.WriteLine("Full list of animals:");
+    foreach (var animal in animalsCollection.Find(a => true).ToList())
+    {
+        Console.WriteLine(animal.Description);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("List of 10 most old animals:");
+    foreach (var animal in animalsCollection.Find(a => true).SortBy(a => a.DateOfBirth).Limit(10).ToList())
+    {
+        Console.WriteLine(animal.Description);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Rabbits in alphabetical order:");
+    foreach (var animal in animalsCollection.Find(a => a.Species == "Rabbit").SortBy(a => a.Name).ToList())
+    {
+        Console.WriteLine(animal.Description);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Animals, who's name starts with 'M', sorted by species:");
+    foreach (var animal in animalsCollection.Find(a => a.Name.StartsWith("M")).SortBy(a => a.Species).ToList())
+    {
+        Console.WriteLine(animal.Description);
+    }
+}
